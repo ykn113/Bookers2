b7e49950c821141b7c35e01f7f95821f4b3fc7a5
ykn113 <yukinosnj13@gmail.com>
2021-06-28 08:42:13 +0000

    [Add]Top,Aboutレイアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_26_071633) do
+ActiveRecord::Schema.define(version: 2021_06_28_074211) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_06_26_071633) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
