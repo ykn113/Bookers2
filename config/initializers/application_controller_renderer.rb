0571f57e77eb1388958fdb683911339e0484799a
ykn113 <yukinosnj13@gmail.com>
2021-07-02 08:21:33 +0000

    [Add]flash message

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '632ba8be00b42e14df0eebdd41260af284bde325ad0f4d8eb1237061cd5e671106b08d169934aadb835b022fcadee5a14bc6608d10b5c822014015b0923834a5'
