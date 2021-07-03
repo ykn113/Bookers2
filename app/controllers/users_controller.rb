b9b406c7015f15097a26b929053327bab5f22908
ykn113 <yukinosnj13@gmail.com>
2021-07-03 05:48:49 +0000

    [Add]flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,13 +17,16 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(current_user.id)
+    if @user.update(user_params)
+      redirect_to user_path(current_user.id), notice:'You have updated user successfully.'
+    else
+      render :user_path
+    end
   end
   
   private
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_074211) do
+ActiveRecord::Schema.define(version: 2021_07_03_044035) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_06_28_074211) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
