163a2c4a76101aff4333e8e995c0f6a05c2763e3
ykn113 <yukinosnj13@gmail.com>
2021-08-03 08:36:23 +0000

    応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
+ 
   
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
