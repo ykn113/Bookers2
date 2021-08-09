4fe4ee71f32e9090510711dfd58a4ca047052505
ykn113 <yukinosnj13@gmail.com>
2021-08-09 05:45:08 +0000

    応用課題7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  is_impressionable 
+  is_impressionable
 
   belongs_to :user
 
@@ -12,9 +12,8 @@ class Book < ApplicationRecord
   validates :category, presence: true
 
   has_many :favorites, dependent: :destroy
-  # 7a追加
   has_many :favorited_users, through: :favorites, source: :user
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
@@ -39,4 +38,9 @@ class Book < ApplicationRecord
     Book.where(['category LIKE ?', "%#{search_word}%"])
   end
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
+  
 end
